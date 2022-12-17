a26abd53517a81d7f37f57195a6153aaae7e1d54
Zeeshan Asghar <thezeeshanasghar@gmail.com>
2022-12-17 08:42:24 +0500

    fix mysql connectivity issue

--- Data/Entities/Vaccine.cs
+++ Data/Entities/Vaccine.cs
@@ -4,7 +4,7 @@ public class Vaccine
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int MinAge { get; set; }
-    public int MaxAge { get; set; }
-    public int? MinGap { get; set; }
+    // public int MinAge { get; set; }
+    // public int MaxAge { get; set; }
+    // public int? MinGap { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
 using api.fernflowers.com.Data;
 using Microsoft.EntityFrameworkCore;
-// using MySQL.Data.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<VaccineDBContext>(options =>
-{
-    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+
+builder.Services.AddDbContext<VaccineDBContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(connectionString, serverVersion)
+        // The following three options help with debugging, but should
+        // be changed or removed for production.
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
 
 var app = builder.Build();
 
