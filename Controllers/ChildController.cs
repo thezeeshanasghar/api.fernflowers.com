e9ccff9b890d8d8bf2ca02310ed58619b2a625d7
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2023-04-05 21:15:12 +0500

    search update

--- Controllers/ChildController.cs
+++ Controllers/ChildController.cs
@@ -7,6 +7,10 @@ using api.fernflowers.com.ModelDTO;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Http.Extensions;
 using System.Globalization;
+using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+
 
 namespace api.fernflowers.com.Controllers
 {
@@ -68,16 +72,91 @@ namespace api.fernflowers.com.Controllers
         // }
 
         [HttpGet("search-by-doctor-name")]
-        public ActionResult<IEnumerable<Child>> SearchByDoctorName(string doctorName, string Name, string City, string Gender, DateTime DOB)
+        public ActionResult<IEnumerable<Child>> SearchByDoctorName( [SwaggerParameter(Required = false)] string? doctorName=null,[SwaggerParameter(Required = false)] string Name=null,[SwaggerParameter(Required = false)] string? City=null,[SwaggerParameter(Required = false)] string? Gender=null,[SwaggerParameter(Required = false)] DateTime? DOB=null)
         {
             var patients = _db.Childs.Join(_db.Doctors, p => p.DoctorId, d => d.Id, (p, d) => new { Child = p, Doctor = d })
-                .Where(pd => pd.Doctor.Name.Contains(doctorName) || pd.Child.Name == Name || pd.Child.City == City || pd.Child.Gender == Gender || pd.Child.DOB.Date == DOB)
+                .Where(pd =>
+                (doctorName==null || pd.Doctor.Name.Contains(doctorName)) ||
+                (Name==null || pd.Child.Name == Name ) &&
+                (City==null || pd.Child.City == City) && 
+                (Gender==null ||pd.Child.Gender == Gender) &&
+                (DOB ==null ||  pd.Child.DOB.Date == DOB))
                 .Select(pd => pd.Child)
                 .ToList();
 
 
             return Ok(patients);
         }
+    //  [HttpGet]
+    // public IActionResult Search([FromQuery] string Name = "",
+    //         [FromQuery] string City = "",
+    //         [FromQuery] DateTime DOB=default,
+    //         [FromQuery] string Gender = "",
+    //         [FromQuery] string DoctorName = "")
+    // {
+    //     var children = _db.Childs.AsQueryable();
+
+    //     if (!string.IsNullOrEmpty(Name))
+    //     {
+    //         children = children.Where(c => c.Name.Contains(Name));
+    //     }
+
+    //     if (!string.IsNullOrEmpty(City))
+    //     {
+    //         children = children.Where(c => c.City == City);
+    //     }
+    //     if (!string.IsNullOrEmpty(Gender))
+    //     {
+    //         children = children.Where(c => c.Gender == Gender);
+    //     }
+
+    //     if (DOB != DateTime.MinValue)
+    //     {
+    //         children = children.Where(c => c.DOB.Date == DOB.Date);
+    //     }
+
+    //     if (!string.IsNullOrEmpty(DoctorName))
+    //     {
+    //         // Find the doctor with the specified ID
+            
+    //         var doctor = _db.Doctors.FirstOrDefault(d => d.Name == DoctorName);
+
+    //         if (doctor != null)
+    //         {
+                
+    //             // Filter the children queryable to only include children whose DoctorId matches the specified ID
+    //             // children = children.Where(c => c.DoctorId == );
+    //             var patients = _db.Childs.Join(_db.Doctors, p => p.DoctorId, d => d.Id, (p, d) => new { Child = p, Doctor = d });
+
+    //             // Create a result object that includes the doctor's name and the filtered children
+    //             var search = new
+    //             {
+    //                 DoctorName = doctor.Name,
+    //                 Children = patients.ToList()
+    //             };
+
+    //             return Ok(search);
+    //         }
+    //         else
+    //         {
+    //             // If the doctor ID is invalid, return an empty result
+    //             return Ok(new { DoctorName = "", Children = new List<Child>() });
+    //         }
+    //     }
+    //     else
+    //     {
+    //         // If no doctor ID was specified, return all children
+    //         var search = new
+    //         {
+    //             DoctorName = "",
+    //             Children = children.ToList()
+    //         };
+
+    //         return Ok(search);
+    //     }
+    // }
+
+
 
         // [HttpGet("{name}")]
 
