afebfc9bf82c435f775e3521dc7ff69e7f129be0
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2023-07-13 23:51:56 -0700

    clinic timing session and day in string

--- Controllers/ClinictimingController.cs
+++ Controllers/ClinictimingController.cs
@@ -29,15 +29,15 @@ namespace api.fernflowers.com.Controllers
             {
                 var clinicTimings = await _db.ClinicTimings.ToListAsync();
 
-                var jsonSettings = new JsonSerializerSettings
-                {
-                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                    Converters = { new StringEnumConverter() }
-                };
+                // var jsonSettings = new JsonSerializerSettings
+                // {
+                //     ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                //     Converters = { new StringEnumConverter() }
+                // };
 
-                var convertedTimings = JsonConvert.SerializeObject(clinicTimings, Formatting.None, jsonSettings);
+                // var convertedTimings = JsonConvert.SerializeObject(clinicTimings, Formatting.None, jsonSettings);
 
-                return Ok(convertedTimings);
+                return Ok(clinicTimings);
             }
             catch (Exception ex)
             {
@@ -60,40 +60,49 @@ namespace api.fernflowers.com.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
+ 
+
+        [Route("api/clintimings/AddorUpdate/{clinicId}")]
         [HttpPost]
-        public async Task<IActionResult> PostNew([FromBody] List<string> data)
+        public async Task<IActionResult> AddOrUpdateClinicTimings(long clinicId, [FromBody] List<ClinicTiming> clinicTimings)
         {
             try
             {
-                List<ClinicTiming> clinictimings = new List<ClinicTiming>();
-                foreach (var item in data)
+                if (clinicTimings == null || !clinicTimings.Any())
                 {
-                    var clinictiming = JsonConvert.DeserializeObject<ClinicTiming>(item);
-                    _db.ClinicTimings.Add(clinictiming);
+                    return BadRequest("No clinic timings provided.");
                 }
-                await _db.SaveChangesAsync();
-                return Ok(new { Message = "Sucessfully Added" });
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
-        }
 
-        [HttpPut]
-        public async Task<IActionResult> PutAsync([FromRoute] long id, [FromBody] ClinicTiming clinictimingToUpdate)
-        {
-            try
-            {
-                if (id != clinictimingToUpdate.Id)
-                    return BadRequest();
-                var dbClinic = await _db.ClinicTimings.FindAsync(id);
-                if (dbClinic == null)
-                    return NotFound();
+                foreach (var ct in clinicTimings)
+                {
+                    // Check if a ClinicTiming with the same day and session already exists
+                    var existingClinicTiming = await _db.ClinicTimings.FirstOrDefaultAsync(c =>
+                        c.Day == ct.Day && c.Session == ct.Session && c.ClinicId == clinicId);
 
-                _db.ClinicTimings.Update(clinictimingToUpdate);
+                    if (existingClinicTiming != null)
+                    {
+                        // Update existing ClinicTiming entity
+                        existingClinicTiming.StartTime = ct.StartTime;
+                        existingClinicTiming.EndTime = ct.EndTime;
+                    }
+                    else
+                    {
+                        // Add new ClinicTiming entity
+                        var newClinicTiming = new ClinicTiming
+                        {
+                            Day = ct.Day,
+                            Session = ct.Session,
+                            StartTime = ct.StartTime,
+                            EndTime = ct.EndTime,
+                            ClinicId = clinicId
+                        };
+                        _db.ClinicTimings.Add(newClinicTiming);
+                    }
+                }
+
                 await _db.SaveChangesAsync();
-                return NoContent();
+
+                return Ok();
             }
             catch (Exception ex)
             {
@@ -103,7 +112,7 @@ namespace api.fernflowers.com.Controllers
 
         [Route("{id}")]
         [HttpDelete]
-        public async Task<IActionResult> DeleteAsync([FromRoute] int id)
+        public async Task<IActionResult> DeleteAsync([FromRoute] long id)
         {
             try
             {
@@ -122,18 +131,42 @@ namespace api.fernflowers.com.Controllers
             }
         }
 
-        [HttpPatch("{id}")]
-        public async Task<IActionResult> PatchAsync([FromRoute] int id, [FromBody] JsonPatchDocument<ClinicTiming> patchDocument)
+        [Route("api/clintimings/{clinicId}")]
+        [HttpPatch]
+        public async Task<IActionResult> UpdateClinicTimings(long clinicId, [FromBody] List<ClinicTiming> updatedTimings)
         {
             try
             {
-                var dbClinictime = await _db.ClinicTimings.FindAsync(id);
-                if (dbClinictime == null)
+                if (updatedTimings == null || !updatedTimings.Any())
+                {
+                    return BadRequest("No updated clinic timings provided.");
+                }
+
+                var timingIds = updatedTimings.Select(t => t.Id).ToList();
+
+                var existingTimings = await _db.ClinicTimings.Where(t => timingIds.Contains(t.Id) && t.ClinicId == clinicId).ToListAsync();
+
+                if (existingTimings == null || existingTimings.Count == 0)
                 {
                     return NotFound();
                 }
-                patchDocument.ApplyTo(dbClinictime);
+
+                foreach (var updatedTiming in updatedTimings)
+                {
+                    var existingTiming = existingTimings.FirstOrDefault(t => t.Id == updatedTiming.Id);
+
+                    if (existingTiming != null)
+                    {
+                        existingTiming.Day = updatedTiming.Day;
+                        existingTiming.Session = updatedTiming.Session;
+                        existingTiming.StartTime = updatedTiming.StartTime;
+                        existingTiming.EndTime = updatedTiming.EndTime;
+                        existingTiming.ClinicId = updatedTiming.ClinicId;
+                    }
+                }
+
                 await _db.SaveChangesAsync();
+
                 return NoContent();
             }
             catch (Exception ex)
@@ -141,6 +174,25 @@ namespace api.fernflowers.com.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
+        // [HttpPatch("{id}")]
+        // public async Task<IActionResult> PatchAsync([FromRoute] long id, [FromBody] JsonPatchDocument<ClinicTiming> patchDocument)
+        // {
+        //     try
+        //     {
+        //         var dbClinictime = await _db.ClinicTimings.FindAsync(id);
+        //         if (dbClinictime == null)
+        //         {
+        //             return NotFound();
+        //         }
+        //         patchDocument.ApplyTo(dbClinictime);
+        //         await _db.SaveChangesAsync();
+        //         return NoContent();
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         return StatusCode(500, ex.Message);
+        //     }
+        // }
     }
 }
 
