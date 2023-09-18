06b7d41a9eda43375164ea9c58c4fdeaff49185c
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2023-09-18 08:04:36 -0700

    Update AdminSchedule.cs

--- Data/Entities/AdminSchedule.cs
+++ Data/Entities/AdminSchedule.cs
@@ -8,7 +8,6 @@ namespace api.fernflowers.com.Data.Entities;
 public class AdminSchedule
 {
     public long Id { get; set; }
-    
     public System.DateOnly Date { get; set; }
     public long DoseId { get; set; }
     public virtual Dose Dose {get;set;}
