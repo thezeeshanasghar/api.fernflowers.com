00558c6fea4f6289517908cc836ffe340d0c81c5
thezeeshanasghar <thezeeshanasghar@gmail.com>
2023-10-02 22:28:28 +0500

    add UseDeveloperExceptionPage

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 // }
