96b8e2071dfdfac1f196d17e28a5364af36a1e7d
thezeeshanasghar <thezeeshanasghar@gmail.com>
2023-10-02 20:04:15 +0500

    fix cors error

--- Program.cs
+++ Program.cs
@@ -11,57 +11,35 @@ builder.Services.AddControllers(options => options.ModelBinderProviders.Insert(0
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder => { builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader(); }));
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "api.fernflowers.com", Version = "v1" });
-});
+builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 31));
 
-
-
-
 builder.Services.AddDbContext<VaccineDBContext>(
-    options => options
-    
+    dbContextOptions => dbContextOptions
         .UseMySql(connectionString, serverVersion)
         // The following three options help with debugging, but should
         // be changed or removed for production.
         .LogTo(Console.WriteLine, LogLevel.Information)
         .EnableSensitiveDataLogging()
         .EnableDetailedErrors()
-        );
-        builder.Services.AddControllersWithViews();
+);
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "api.fernflowers.com v1");
-         c.RoutePrefix = string.Empty;
-    });
-}
+    app.UseSwaggerUI();
+// }
 
-// app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.UseCors("corsapp");
-
-using (var scope = app.Services.CreateScope())
-{
-    var serviceProvider = scope.ServiceProvider;
-    var dbContext = serviceProvider.GetRequiredService<VaccineDBContext>();
-    dbContext.Database.EnsureCreated(); // Optional: Ensure the database is created before applying the changes
-    dbContext.Database.Migrate(); // Optional: Apply pending migrations before applying the changes
-}
-
 app.Run();
