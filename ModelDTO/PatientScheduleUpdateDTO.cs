8c8aeeb0cbd30580651d1d33baa1c8289d457eee
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2024-03-05 16:28:01 +0500

    update patientdto

--- ModelDTO/PatientScheduleUpdateDTO.cs
+++ ModelDTO/PatientScheduleUpdateDTO.cs
@@ -8,6 +8,9 @@ public class PatientScheduleUpdateDTO
     public string CurrentDate { get; set; }
     public bool IsDone { get; set; }
     public string GivenDate { get; set; }
-    public long BrandId { get; set; }
+    public long? BrandId { get; set; }
+    public double? Height { get; set; }
+    public double? Weight { get; set; }
+    public double? OFC { get; set; }
     public bool IsSkip { get; set; }
 }
